8d0657035c5a89faebdb46c982ff39bf680f1753
wenbingma <wenbing.ma@qq.com>
2016-09-01 17:44:59 +0800

    test project
    DA14580 sample test project

--- README.md
+++
@@ -1,2 +0,0 @@
-# shop-home
-shop home project
---
+++ project/src/main.c
@@ -0,0 +1,25 @@
+ /*
+ ****************************************************************************************
+ *
+ * @file DA14580_examples.c
+ *
+ * @brief DA14580 Peripheral Examples for DA14580 SDK
+ *
+ * Copyright (C) 2012. Dialog Semiconductor Ltd, unpublished work. This computer 
+ * program includes Confidential, Proprietary Information and is a Trade Secret of 
+ * Dialog Semiconductor Ltd.  All use, disclosure, and/or reproduction is prohibited 
+ * unless authorized in writing. All Rights Reserved.
+ *
+ * <bluetooth.support@diasemi.com> and contributors.
+ *
+ ****************************************************************************************
+ */
+#include <stdio.h>
+#include "global_io.h"
+#include "peripherals.h"
+
+int main (void)
+{	
+	periph_init();
+	while(1);
+}
